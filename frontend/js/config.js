40b14dda22553ed5c3028f3a04c57c3052cfcec2
Vinyurchin <diegoc.g56@gmail.com>
2026-01-15 14:46:58 -0600

    Actualizar URL backend a Heroku

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -6,6 +6,6 @@
 // Detecta automáticamente el entorno
 const API_BASE_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
     ? "http://localhost:8000"
-    : "https://explorador-ml-backend.onrender.com";
+    : "https://explorador-ml-c0e7fc5ed3d6.herokuapp.com";
 
 // const API_BASE_URL = "https://scratch-makes-shown-implies.trycloudflare.com"; // cloudflared 
